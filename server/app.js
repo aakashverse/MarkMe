b1b0197306c11314d30a44b89c5650742e094b43
planet-akash <akashyadavakash03@gmail.com>
2026-01-27 20:55:11 +0530

    updated cors for production

--- server/app.js
+++ server/app.js
@@ -11,12 +11,9 @@ const app = express();
 // middlewares
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(
-  cors({
-    origin: "http://localhost:5173",
-    credentials: true,
-  })
-);
+app.use(cors());
+app.options("*", cors());
+
 
 // db connect
 mongoose
